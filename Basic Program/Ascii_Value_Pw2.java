965cf90ef24422442d43c48c2153f16c8f61c8fa
Codes-World <aman.soni6200@gmail.com>
2025-01-19 21:47:21 +0530

    Files Addes

---
+++ Basic Program/Ascii_Value_Pw2.java
@@ -0,0 +1,7 @@
+public class Ascii_Value_Pw2 {
+    public static void main(String[] args){
+        char value = 'U';
+        int ascii = value;
+        System.out.println("ASCII value of "+ value + ": "+ascii);
+    }
+}
---
+++ Basic Program/Cube_Pw4.java
@@ -0,0 +1,6 @@
+ public class Cube_Pw4 {
+        public static void main(String[] args){
+            int n = 4;
+            System.out.println("The cube of "+ n +" is "+(n*n*n));
+        }
+}
