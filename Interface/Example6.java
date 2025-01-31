e2a127967c5280b196963d28bed7046ec5767393
Code-World <aman.soni6200@gmail.com>
2025-01-31 19:42:21 +0530

    New Program

---
+++ Interface/Example6.java
@@ -0,0 +1,28 @@
+public class Example6{
+	public static void main(String[] args) {
+		Calculator calc;
+		calc = new CalculatorImpl();
+		calc.add(10,20);
+		calc.sub(10,3);
+	}
+}
+
+interface ICalculator{
+	public void add(int a,int b);
+}
+
+class CalculatorDemo{
+	public void sub(int a,int b){
+		System.out.println("The sub is :: "+(a-b));
+	}
+}
+
+abstract class Calculator extends CalculatorDemo implements ICalculator{}
+
+class CalculatorImpl extends Calculator{
+	@Override
+	public void add(int a,int b){
+		System.out.println("The sum is :: "+(a+b));
+	}
+}
+
