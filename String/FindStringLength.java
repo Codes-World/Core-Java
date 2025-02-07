b800125de74bdebabbdaec550fb54a70d0738781
Code-World <aman.soni6200@gmail.com>
2025-02-07 23:07:29 +0530

    New Problem

---
+++ String/FindStringLength.java
@@ -0,0 +1,7 @@
+public class FindStringLength{
+	public static void main(String[] args){
+		String str1 = "refrigerator";
+		int length = str1.length();
+		System.out.println("The length of the refrigerator is "+length);
+	}
+}
